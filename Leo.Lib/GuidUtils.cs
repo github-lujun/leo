f1d110e93e4aa872d74d8e4bd4118be98a70dbb0
lujun <838587241@qq.com>
2019-03-29 10:34:04 +0800

    add lib and tests

---
+++ Leo.Lib/GuidUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Leo.Lib
+{
+    /// <summary>
+    /// GUID操作类
+    /// </summary>
+    public static class GuidUtils
+    {
+        ///<summary>
+        /// 返回 GUID 用于数据库操作，特定的时间代码可以提高检索效率
+        /// </summary>
+        /// <returns>COMB (GUID 与时间混合型) 类型 GUID 数据</returns>
+        public static Guid NewComb()
+        {
+            byte[] guidArray = System.Guid.NewGuid().ToByteArray();
+            DateTime baseDate = new DateTime(1900, 1, 1);
+            DateTime now = DateTime.Now;
+            TimeSpan days = new TimeSpan(now.Ticks - baseDate.Ticks);
+            TimeSpan msecs = new TimeSpan(now.Ticks - (new DateTime(now.Year, now.Month, now.Day).Ticks));
+
+            byte[] daysArray = BitConverter.GetBytes(days.Days);
+            byte[] msecsArray = BitConverter.GetBytes((long)(msecs.TotalMilliseconds / 3.333333));
+
+            Array.Reverse(daysArray);
+            Array.Reverse(msecsArray);
+
+            Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
+            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+
+            return new System.Guid(guidArray);
+        }
+
+        /// <summary>
+        /// 从 SQL SERVER 返回的 GUID 中生成时间信息
+        /// </summary>
+        /// <param name="guid">包含时间信息的 COMB </param>
+        /// <returns>时间</returns>
+        public static DateTime GetDateFromComb(System.Guid guid)
+        {
+            DateTime baseDate = new DateTime(1900, 1, 1);
+            byte[] daysArray = new byte[4];
+            byte[] msecsArray = new byte[4];
+            byte[] guidArray = guid.ToByteArray();
+
+            Array.Copy(guidArray, guidArray.Length - 6, daysArray, 2, 2);
+            Array.Copy(guidArray, guidArray.Length - 4, msecsArray, 0, 4);
+
+            Array.Reverse(daysArray);
+            Array.Reverse(msecsArray);
+
+            int days = BitConverter.ToInt32(daysArray, 0);
+            int msecs = BitConverter.ToInt32(msecsArray, 0);
+
+            DateTime date = baseDate.AddDays(days);
+            date = date.AddMilliseconds(msecs * 3.333333);
+
+            return date;
+        }
+    }
+}
---
+++ Leo.LibTests/GuidUtilsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leo.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leo.Lib.Tests
+{
+    [TestClass()]
+    public class GuidUtilsTests
+    {
+        [TestMethod()]
+        public void NewCombTest()
+        {
+            Guid guid = GuidUtils.NewComb();
+            Assert.IsNotNull(guid);
+        }
+
+        [TestMethod()]
+        public void GetDateFromCombTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
