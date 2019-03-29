7b5d9e63b73e634c9c33135d735a337ad7cbf9f5
lujun <838587241@qq.com>
2019-03-29 14:57:49 +0800

    mail helper

---
+++ Leo.Lib/MailHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Leo.Lib
+{
+    public class MailHelper
+    {
+        public static void SendMail(String from,String[] to,String subject,String body,String username,String password,String smtp)
+        {
+            SendMail(new MailAddress(from), to, subject, body, username, password,smtp);
+        }
+
+        public static void SendMail(MailAddress from, String[] to, String subject, String body, String username, String password, String smtp)
+        {
+            var mail = new MailMessage();
+            mail.From = from;
+            foreach (var t in to)
+            {
+                mail.To.Add(t);
+            }
+            mail.Subject = subject;
+            mail.Body = body;
+            var client = new SmtpClient(smtp);
+            client.Credentials = new NetworkCredential(username,password);
+            client.Send(mail);
+        }
+    }
+}
---
+++ Leo.LibTests/MailHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leo.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leo.Lib.Tests
+{
+    [TestClass()]
+    public class MailHelperTests
+    {
+        [TestMethod()]
+        public void SendMailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendMailTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
